0b0c3f8bbe2e6fd9305ce54eb71c808e503d7520
Felipe Salomao <lipesalomaoclaf@gmail.com>
2023-02-28 20:12:28 -0300

    feat: a

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { parseCookies } from "nookies";
 export const getAPIClient = (ctx?: any) => {
   const { "doceifancia.auth": token } = parseCookies(ctx);
+  //asdasdsa
   const api = axios.create({
     baseURL: "https://doceinfanciabackend-production.up.railway.app",
     // baseURL: "http://localhost:3050",
